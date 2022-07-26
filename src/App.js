281f860988b0ac20c62dd53d16131274be0462f5
Adam <79875785+alldigits@users.noreply.github.com>
2022-07-26 14:52:58 +0100

    one, created header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 function App() {
+  const name = 'Snoopy'
+  const x = false 
+
+
   return (
-    <div className="App">
+    <div className="container">
       <h1>Hello From React</h1>
+      <h2>Hello {x ? 'Yes' : 'No'}</h2>
     </div>
+  
   );
 }
 
-export default App;
+export default App;  
