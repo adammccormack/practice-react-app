a3e686fef84e3aac6777b48a39b8410da5d4053a
Adam <79875785+alldigits@users.noreply.github.com>
2022-07-26 15:25:35 +0100

    one, added props to header comp

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
+import Header from "./components/Header";
+
 function App() {
-  const name = 'Snoopy'
-  const x = false 
-
-
   return (
     <div className="container">
-      <h1>Hello From React</h1>
-      <h2>Hello {x ? 'Yes' : 'No'}</h2>
+      <Header />
     </div>
-  
-  );
+  )
 }
 
-export default App;  
+export default App;
