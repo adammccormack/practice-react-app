f5187420279eb7cafa8b3e118870cae558d6a25f
Adam <79875785+alldigits@users.noreply.github.com>
2022-07-28 12:32:29 +0100

    one, added new components

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,7 @@ const Button = ({ color, text, onClick }) => {
 
   return (
     <button 
-    onClick={onClick} 
+    onClick={onClick}
     style={{ backgroundColor: color }}
     className='btn'>{ text }
      </button>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,26 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
+const onClick = () => {
+  console.log('Clicked')
+}
+
 const Header = ({ title }) => {
-  const onClick = () => {
-    console.log('Clicked')
-  }
+  
   return (
     <header className='header'>
         <h1>{title}</h1>
-        <Button color='green' text='Add' onClick={onClick}/>
+        <Button color='green' text='Add' onClick={onClick} />
     </header>
-  )   
+  )
 }
 
 Header.defaultProps = {
-    title: 'Task Tracker', 
+    title: 'Task Tracker',
 }
  
 Header.propTypes = {
     title: PropTypes.string.isRequired,
 }
 
-export default Header;
+export default Header
