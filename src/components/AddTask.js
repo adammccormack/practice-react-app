19ea4f3e201ef7a86d33e9e4e38d95c3ebb10d4b
Adam McCormack <79875785+alldigits@users.noreply.github.com>
2022-10-14 00:06:40 +0100

    added add task functionality

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,12 +1,27 @@
 import { useState } from 'react'
 
-const AddTask = () => {
+const AddTask = ({ onAdd }) => {
    const [text, setText] = useState('')
    const [day, setDay] = useState('')
    const [reminder, setReminder] = useState(false)
 
+  const onSubmit = (e) => {
+      e.preventDefault()
+
+      if(!text) {
+         alert('Please add a task')
+         return
+      }
+
+      onAdd({ text, day, reminder })
+
+      setText('')
+      setDay('')
+      setReminder(false)
+  }
+  
   return (
-    <form className='add-form'>
+    <form className='add-form' onSubmit={onSubmit}>
          <div className='form-control'>
             <label>Task</label>
             <input 
@@ -28,11 +43,11 @@ const AddTask = () => {
          <div className='form-control
          form-control-check'>
             <label>Set Reminder</label>
-            <input 
+            <input
                type='checkbox'
+               checked={reminder}
                value={reminder}
                onChange={(e) => setReminder(e.currentTarget.checked)}
-            
             />
          </div>
 
