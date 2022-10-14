68a3ecbac43758e2f13371355e2b9082f662b125
Adam McCormack <79875785+alldigits@users.noreply.github.com>
2022-10-14 01:46:07 +0100

    updated showAdd functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask"
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -26,7 +27,7 @@ function App() {
 ])
 
 const addTask = (task) => {
-  const id = Math.floor(Math.random() * 100  00) + 1
+  const id = Math.floor(Math.random() * 10000) + 1
   const newTask = { id, ...task }
   setTasks([...tasks, newTask])
 }
@@ -41,8 +42,11 @@ const toggleReminder = (id) => {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask}/>
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
        : 'No Tasks Left'}
     </div>
